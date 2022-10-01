b64c22af27d2e1dd5ce34630ee575668157cc6b9
Aman11 <kadakuntlaaman@gmail.com>
2022-10-01 08:56:02 +0530

    Add project files.

---
+++ ConvertListToDataSet/ExtensionMethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertListToDataSet
+{
+    public static class ExtensionMethods
+    {
+        public static DataSet ToDataSet<T>(this List<T> list)
+        {
+            var converted = new DataSet();
+            converted.Tables.Add(NewTable(list));
+            return converted;
+        }
+        private static DataTable NewTable<T>(List<T> list)
+        {
+            PropertyInfo[] propInfo = typeof(T).GetProperties();
+            DataTable table = Table<T>(list, propInfo);
+            IEnumerator<T> enumerator = list.GetEnumerator();
+            while (enumerator.MoveNext())
+                table.Rows.Add(CreateRow<T>(table.NewRow(), enumerator.Current, propInfo));
+            return table;
+        }
+        private static DataRow CreateRow<T>(DataRow row, T listItem, PropertyInfo[] pi)
+        {
+            foreach (PropertyInfo p in pi)
+                row[p.Name.ToString()] = p.GetValue(listItem, null);
+            return row;
+        }
+        private static DataTable Table<T>(List<T> list, PropertyInfo[] pi)
+        {
+            DataTable table = new DataTable();
+            foreach (PropertyInfo p in pi)
+                table.Columns.Add(p.Name, p.PropertyType);
+            return table;
+        }
+
+    }
+}
---
+++ ConvertListToDataSet/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using ConvertListToDataSet;
+using System.Data;
+
+Console.WriteLine("converting list to dataset code...");
+
+List<Employee> employees = new List<Employee>();
+employees.Add(new Employee() { EmployeeName = "aman", EmployeeAge = 24, EmployeeId = 123 });
+employees.Add(new Employee() { EmployeeName = "sruthi", EmployeeAge = 25, EmployeeId = 234 });
+employees.Add(new Employee() { EmployeeName = "akash", EmployeeAge = 39, EmployeeId = 453 });
+employees.Add(new Employee() { EmployeeName = "archana", EmployeeAge = 28, EmployeeId = 234 });
+
+employees.ForEach(emp =>
+{
+    Console.WriteLine($"{emp.EmployeeName}\t{emp.EmployeeAge}\t{emp.EmployeeId}");
+});
+
+DataSet dataSet = employees.ToDataSet();
+
+DataTable dt = dataSet.Tables[0];
+
+//data set
+Console.WriteLine("Displaying Data set");
+foreach (DataRow row in dt.Rows)
+{
+    Console.WriteLine(String.Join(" ",row.ItemArray));
+}
